ca94750ff000c706241c4ca2a2a49515542e3ab1
elisaul batista <elisaulbatista@pursuit.org>
2023-10-03 15:07:36 -0400

    fix depencies

--- src/components/Subtotal.jsx
+++ src/components/Subtotal.jsx
@@ -1,31 +1,27 @@
 import React from "react";
 import "./subtotal.css";
-import CurrencyFormat from "react-currency-format";
 import { useStateValue } from "./StateProvider";
 import { getBasketTotal } from "./Reducer";
 
-
-
 const Subtotal = () => {
-    const [{basket}, dispatch] = useStateValue();
+  const [{ basket }, dispatch] = useStateValue();
+
+
+  const formatAsCurrency = (value) => {
+    return value.toLocaleString("en-US", {
+      style: "currency",
+      currency: "USD"
+    });
+  };
+
   return (
     <div className="subtotal">
-      <CurrencyFormat
-        renderText={(value) => (
-          <>
-            <p>
-              Subtotal ({basket.length} item): <strong>${value}</strong>
-            </p>
-            <small className="subtotal-gift">
-              <input type="checkbox" /> This Order Contains a Gift
-            </small>
-          </>
-        )}
-        decimalScale={2}
-        value={getBasketTotal(basket)}
-        displayType={"text"}
-        thousandSeparator={true}
-      />
+      <p>
+        Subtotal ({basket.length} item): <strong>{formatAsCurrency(getBasketTotal(basket))}</strong>
+      </p>
+      <small className="subtotal-gift">
+        <input type="checkbox" /> This Order Contains a Gift
+      </small>
       <button>Proceed to checkout</button>
     </div>
   );
